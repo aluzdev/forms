1966ecebbe4b5670919f73645ef54dc91e02b588
aluzdev <aluzdev@gmail.com>
2024-02-21 00:57:19 -0700

    componetized form

---
+++ src/components/Form.jsx
@@ -0,0 +1,94 @@
+export const Form = ({
+  handleName,
+  handleFavoriteColor,
+  handleRememberMe,
+  handleAvatar,
+  handleSubmit,
+  name,
+  favoriteColor,
+  rememberMe,
+  avatar,
+}) => {
+  return (
+    <>
+      <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
+        <div className="mb-5">
+          <label
+            htmlFor="name"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your name
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Pochita"
+            required
+            value={name}
+            onChange={handleName}
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="favorite-color"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your favorite-color
+          </label>
+          <input
+            type="text"
+            placeholder="blue"
+            id="favorite-color"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+            value={favoriteColor}
+            onChange={handleFavoriteColor}
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="avatar"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            avatar
+          </label>
+          <input
+            type="text"
+            placeholder="img url"
+            id="avatar"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            required
+            value={avatar}
+            onChange={handleAvatar}
+          />
+        </div>
+
+        <div className="flex items-start mb-5">
+          <div className="flex items-center h-5">
+            <input
+              id="remember"
+              type="checkbox"
+              className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+              checked={rememberMe}
+              onChange={handleRememberMe}
+            />
+          </div>
+          <label
+            htmlFor="remember"
+            className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            Remember me
+          </label>
+        </div>
+
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
+      </form>
+    </>
+  );
+};
